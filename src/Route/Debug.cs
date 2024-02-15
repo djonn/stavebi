25674881d6401962325b6b362f7e6b675b1884f6
Jonas Nielsen <jonas@nielsenjonas.dk>
2024-02-15 17:39:20 +0100

    Add more debug endpoints

--- src/Route/Debug.cs
+++ src/Route/Debug.cs
@@ -1,4 +1,6 @@
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StaveBi.Application;
 using StaveBi.Database;
 
@@ -12,10 +14,24 @@ public static class Debug
     {
       return context.Words.Any(x => x.Value == word);
     });
+
     route.MapGet("/add-word", ([FromServices] GameContext context, [FromQuery] string word) =>
     {
       context.Words.Add(new Model.Word(word));
       context.SaveChanges();
+    });
+
+    route.MapGet("/game-count", ([FromServices] GameContext context) =>
+    {
+      return context.Games.Count();
+    });
+
+    route.MapGet("/find-solutions", ([FromServices] GameContext context, [FromQuery] string id) =>
+    {
+      var firstLetter = id.Substring(0, 1);
+      return context.Words
+        .Where(w => w.Value.Contains(firstLetter) && Regex.IsMatch(w.Value, $"^[{id}]+$"))
+        .Select(w => w.Value);
     });
 
     route.MapGet("/words-by-type", ([FromServices] GameGenerator generator, [FromQuery] string conjugation, [FromQuery] int count, [FromQuery] int offset) =>
@@ -29,6 +45,15 @@ public static class Debug
     {
       var unfilteredWords = generator.ParseWords();
       return unfilteredWords.Any(x => !x.Standardized);
+    });
+
+    route.MapGet("/migration", ([FromServices] GameContext context) =>
+    {
+      return new Dictionary<string, IEnumerable<string>>()
+      {
+        {"all", context.Database.GetMigrations()},
+        {"applied", context.Database.GetAppliedMigrations()}
+      };
     });
   }
 }
