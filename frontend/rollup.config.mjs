52e888163dee5e9bab2ea0bd7422b53684499fd3
Jonas Nielsen <jonas@nielsenjonas.dk>
2024-03-01 15:13:34 +0100

    Copy frontend assets using rollup and allow proper watching

--- frontend/rollup.config.mjs
+++ frontend/rollup.config.mjs
@@ -1,9 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
+import copy from '@rollup-extras/plugin-copy';
 
 export default {
     input: 'src/script.ts',
     output: {
         format: 'cjs'
     },
-    plugins: [typescript()]
+    plugins: [
+        typescript(),
+        copy('assets/*')
+    ],
 };
