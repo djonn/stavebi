7e08d18a92dc8a1c603cac75969f966a98d56941
Jonas Nielsen <djonnneller@gmail.com>
2024-04-28 22:59:45 +0200

    Fix problem with inconsisten url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,17 +2,17 @@ import { Game } from "./game";
 
 export type GuessResponse =
   | { success: true }
-  | { success: false, reason: string}
+  | { success: false; reason: string };
 
 export class Api {
-
   private static cache: Record<string, any> = {};
 
   private static async cachedFetch(url: string): Promise<any> {
-    if(Api.cache[url]) return Api.cache[url];
+    if (Api.cache[url]) return Api.cache[url];
 
-    const response = await fetch(url)
-      .then(response => response.json());
+    const response = await fetch(`${document.location.href}/${url}`).then(
+      (response) => response.json()
+    );
 
     Api.cache[url] = response;
     return response;
@@ -20,8 +20,8 @@ export class Api {
 
   public static async newGame(): Promise<Game> {
     const gamesList = await Api.cachedFetch("/api/games.json");
-    
-    const i = Math.floor(Math.random()*gamesList.length);
+
+    const i = Math.floor(Math.random() * gamesList.length);
     const gameDto = gamesList[i];
     return new Game(gameDto.Letters, gameDto.TotalScore);
   }
@@ -29,7 +29,7 @@ export class Api {
   public static async guess(game: Game, guess: string): Promise<GuessResponse> {
     const solutions = await Api.cachedFetch(`/api/game-${game.letters}.json`);
     return solutions.includes(guess)
-        ? { success: true }
-        : { success: false, reason: "WORD_DOES_NOT_EXIST" };
+      ? { success: true }
+      : { success: false, reason: "WORD_DOES_NOT_EXIST" };
   }
 }
