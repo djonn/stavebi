720329ac7bc72e24f15df1a545e52356a4eb083f
Jonas Nielsen <djonnneller@gmail.com>
2024-05-11 13:02:08 +0200

    Add lemma frequency to db

--- src/Migrations/20240511101612_AddLemmaFrequency.Designer.cs
+++ src/Migrations/20240511101612_AddLemmaFrequency.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StaveBi.Database;
 
@@ -10,9 +11,11 @@ using StaveBi.Database;
 namespace SpellingBee.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20240511101612_AddLemmaFrequency")]
+    partial class AddLemmaFrequency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ src/Migrations/20240511101612_AddLemmaFrequency.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpellingBee.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLemmaFrequency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "LemmaFrequency",
+                table: "Words",
+                type: "REAL",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LemmaFrequency",
+                table: "Words");
+        }
+    }
+}
